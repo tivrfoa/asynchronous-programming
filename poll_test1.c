e7623aac5d79d25efdec422a250f0b2dceebd1b3
Leandro Coutinho <lescoutinhovr@gmail.com>
2023-03-25 13:47:08 -0300

    async with poll file descriptor

--- poll_test1.c
+++ poll_test1.c
@@ -6,10 +6,27 @@
 int main() {
 
     char name[255];
+    int counter = 0;
+
+    struct pollfd mypoll;
+
+    memset(&mypoll, 0, sizeof(mypoll));
+    mypoll.fd = 0;
+    mypoll.events = POLLIN;
 
     printf("Type in your name:\n");
-    read(0, name, sizeof(name));
-    printf("Hello, %s\n", name);
+
+    while (1) {
+        if (poll(&mypoll, 1, 100) == 1) {
+            read(0, name, sizeof(name));
+            printf("Hello, %s\n", name);
+            break;
+        } else {
+            ++counter;
+        }
+    }
+
+    printf("It took you %d ms to type in your name\n", 100 * counter);
 
     return 0;
 }
